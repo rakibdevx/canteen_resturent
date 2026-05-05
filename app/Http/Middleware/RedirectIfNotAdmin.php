c85faa0322a75b238cd67cf60abc7f592420e11c
rakibdevx <rakib042002@gmail.com>
2026-05-05 23:30:37 +0600

    Done

--- app/Http/Middleware/RedirectIfNotAdmin.php
+++ app/Http/Middleware/RedirectIfNotAdmin.php
@@ -9,10 +9,10 @@ class RedirectIfNotAdmin
 {
     public function handle($request, Closure $next)
     {
-        if (!Auth::check()) {
-            return redirect()->route('auth.login')->withErrors(['error' => 'Access denied. You must log in to continue.']);
+        if (Auth::check() && Auth::user()->role == "admin") {
+            return $next($request);
         }
 
-        return $next($request);
+        return redirect()->route('auth.login')->with('error', 'You do not have permission to access this page.');
     }
 }
